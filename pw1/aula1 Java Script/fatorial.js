d989eaa47417afb9ff625d75ec21febea14f2877
Peixinss <renan.a.peixinho@gmail.com>
2026-06-11 20:40:16 -0300

    Add files via upload

---
+++ pw1/aula1 Java Script/fatorial.js
@@ -0,0 +1,5 @@
+function fat(n){
+    if(n<=1){return 1;}
+    return n * fat(n-1);
+}
+console.log(fat(4));
---
+++ pw1/aula1 Java Script/tabuada.js
@@ -0,0 +1,8 @@
+for( let t = 2; t <=10; t++){
+    console.log("Tabuada do " + t+ " : ")
+    for(let i = 0; i<=10; i++){
+        let res = t * i;
+        console.log(  t +" X " + i + " = " + res);
+    }
+    console.log("")
+}
